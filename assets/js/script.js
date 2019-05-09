f99f5a9272c30e182b07be33746946b71e097668
core.editor <andreeavillatte@gmail.com>
2019-05-09 16:09:55 +0200

    après corrections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
+// On a declaré une variable pour avoir l'application, on cree un modul qui s'appelle schoolapp
 var schoolApp = angular.module('schoolApp',[]);
-
+// on a cree un controleur studentCtrl avec une dependence SERVICE scope. Ce controlleur est atache au modele schoolApp
 schoolApp.controller('studentsCtrl',function($scope){
+//  Modèle, comme un tunel
+// ensuite on ouvre un tableau
   $scope.firstName = 'Émilie';
   $scope.lastName = 'Gérard';
   $scope.age = '17';
